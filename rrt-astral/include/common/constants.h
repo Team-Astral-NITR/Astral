9265c67989416dbc24503ea4aa1dcf5052cd874e
aatishrana495 <aatishrana495@gmail.com>
2020-08-09 15:38:27 +0530

    simulator added

--- rrt-astral/include/common/constants.h
+++ rrt-astral/include/common/constants.h
@@ -4,10 +4,10 @@
 const float WORLD_WIDTH = 1000.0;
 const float WORLD_HEIGHT = 1000;
 const float BOT_RADIUS = 15;
-const float START_POS_X = 30.0;
-const float START_POS_Y = 30.0;
-const float END_POS_X = 600;
-const float END_POS_Y = 450;
+const float START_POS_X = 400;
+const float START_POS_Y = 450;
+const float END_POS_X = 900;
+const float END_POS_Y = 200;
 const float NODE_RADIUS = 2;
 const float END_DIST_THRESHOLD = 15.0;
 const float BOT_CLEARANCE = 1.5 * BOT_RADIUS;
